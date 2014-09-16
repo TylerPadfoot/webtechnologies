e5ac28a37f4dd98cd86c72efab0155827d88134b
unknown <tyler.padfoot@live.com>
2014-09-16 14:55:27 -0700

    initialized web project

---
+++ www/routes/index.js
@@ -0,0 +1,14 @@
+/**
+ * Created by Shawn on 2014-09-16.
+ */
+    //this is the file that registers all the apis for the server - what to respond when a certain request is called.
+    
+var express = require('express');
+var router = express.Router();
+
+/* GET home page. */
+router.get('/', function(req, res) {
+    res.render('index', { title: 'Express' });
+});
+/* GET Hello World page. */
+module.exports = router;
